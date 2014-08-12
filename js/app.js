df5c67f46485ece6c9241cb3d768bac749ee16be
BHARGAV GOLLA <bhargav.golla@gmail.com>
2014-08-12 16:58:33 -0400

    Prevent Default on link click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
+function showDesc(e){
+	e.preventDefault();
+	/*var homeDiv = $(this).parent().parent();
+	homeDiv.toggle();
+	homeDiv.next().toggle();*/
+}
+
+function showHome(e){
+	e.preventDefault();
+	/*var descDiv = $(this).parent().parent();
+	descDiv.toggle();
+	descDiv.prev().toggle();*/
+}
+
 $(document).ready(function(){
+	$('a.showDesc').click(showDesc);
+	$('a.showHome').click(showHome);
 	// Code inspired from : http://codepen.io/johnmotyljr/pen/tkipE
 	$('.flip').on({
 	    click: function() {
