8afc84c9f965dccc406ccc4d12a5c481d9127eb1
BHARGAV GOLLA <bhargav.golla@gmail.com>
2014-08-12 16:42:43 -0400

    Flipping Experience

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
-function showDesc(e){
-	e.preventDefault();
-	var homeDiv = $(this).parent().parent();
-	homeDiv.toggle();
-	homeDiv.next().toggle();
-}
-
-function showHome(e){
-	e.preventDefault();
-	var descDiv = $(this).parent().parent();
-	descDiv.toggle();
-	descDiv.prev().toggle();
-}
-
 $(document).ready(function(){
-	$('a.showDesc').click(showDesc);
-	$('a.showHome').click(showHome);
+	// Code inspired from : http://codepen.io/johnmotyljr/pen/tkipE
+	$('.flip').on({
+	    click: function() {
+	        $('.card', this).toggleClass('flipped');
+	    },
+	    mouseleave: function() {
+		   	if ($('.card', this).hasClass('flipped')) {
+		   	    $('.card', this).removeClass('flipped');
+		    }
+		}
+	});
 });
