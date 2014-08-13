b692f76b3430117a19b965b126bb79057ddcd22a
BHARGAV GOLLA <bhargav.golla@gmail.com>
2014-08-13 19:35:31 -0400

    Card heights redefined. Pick height of largest card and set

--- js/app.js
+++ js/app.js
@@ -12,6 +12,37 @@ function showHome(e){
 	descDiv.prev().toggle();*/
 }
 
+function setCardHeights(lineHeight) {
+	console.log("Being set");
+	var exps = document.getElementsByClassName('flipExp');
+	var ports = document.getElementsByClassName('flipPort');
+	var maxHeightExp = 0;
+	var maxHeightPort = 0;
+	var temp = 0;
+	for (var i = exps.length - 1; i >= 0; i--) {
+		temp = exps[i].getElementsByTagName('p')[0].offsetHeight + exps[i].getElementsByTagName('header')[0].offsetHeight;
+		if (maxHeightExp < temp) {
+			maxHeightExp = temp;
+		}
+	};
+	maxHeightExp = maxHeightExp*1.75/lineHeight;
+	for (var i = exps.length - 1; i >= 0; i--) {
+		exps[i].style.height = maxHeightExp+"em";
+	};
+
+	for (var i = ports.length - 1; i >= 0; i--) {
+		temp = ports[i].getElementsByTagName('p')[0].offsetHeight + ports[i].getElementsByTagName('header')[0].offsetHeight;
+		if (maxHeightPort < temp) {
+			maxHeightPort = temp;
+		}
+	};
+	maxHeightPort = maxHeightPort*1.75/lineHeight;
+	for (var i = ports.length - 1; i >= 0; i--) {
+		ports[i].style.height = maxHeightPort+"em";
+	};
+
+}
+
 $(document).ready(function(){
 	$('a.showDesc').click(showDesc);
 	$('a.showHome').click(showHome);
@@ -25,5 +56,13 @@ $(document).ready(function(){
 		   	    $('.card', this).removeClass('flipped');
 		    }
 		}
+	});
+
+	$(window).resize(function() {
+		lineHeight = 37.33;
+		if ($(window).width() < 640) {
+			lineHeight = 32.667;
+		}
+		setCardHeights(lineHeight);
 	});
 });
